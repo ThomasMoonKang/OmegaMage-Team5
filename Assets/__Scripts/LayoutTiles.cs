94442b95cdc0342029a844799ae674eab91f91bb
Thomas Kang <tmkang201@gmail.com>
2015-04-29 02:01:19 -0400

    Thomas Additions
    Fixed movement for mage and spiders.
    Added an air spell.
    Added a death UI Screen.
    Fixed collider and spell issues with spell circles. (Could not cast
    spell if you start dragging on a spinning circle)

--- Assets/__Scripts/LayoutTiles.cs
+++ Assets/__Scripts/LayoutTiles.cs
@@ -7,6 +7,14 @@ public class TileTex {
 	public string str;
 	public Texture2D tex;
 }
+[System.Serializable]
+public class EnemyDef {
+	// This class enables us to define various enemies
+	public string str;
+	public GameObject go;
+}
+
+
 public class LayoutTiles : MonoBehaviour {
 	static public LayoutTiles S;
 	public TextAsset roomsText; // The Rooms.xml file
@@ -14,11 +22,18 @@ public class LayoutTiles : MonoBehaviour {
 	// ^ roomNumber as string allows encoding in the XML & rooms 0-F
 	public GameObject tilePrefab; // Prefab for all Tiles
 	public TileTex[] tileTextures; // A list of named textures for Tiles
+	public GameObject portalPrefab; // Prefab for the portals between rooms
+
+	public EnemyDef[] enemyDefinitions; // Prefabs for Enemies
+
 	public bool ________________;
+
+	private bool firstRoom = true; //Is this the first room built?
 	public PT_XMLReader roomsXMLR;
 	public PT_XMLHashList roomsXML;
 	public Tile[,] tiles;
 	public Transform tileAnchor;
+
 	void Awake() {
 		S = this; // Set the Singleton for LayoutTiles
 		// Make a new GameObject to be the TileAnchor (the parent transform of
@@ -43,8 +58,43 @@ public class LayoutTiles : MonoBehaviour {
 		// Return null if nothing was found
 		return(null);
 	}
+
+	// Build a room based on room number. This is an alternative version of
+	//BuildRoom that grabs roomXML based on <room> num.
+	public void BuildRoom(string rNumStr) {
+		PT_XMLHashtable roomHT = null;
+
+		for (int i=0; i<roomsXML.Count; i++) {
+			PT_XMLHashtable ht = roomsXML[i];
+			if (ht.att("num") == rNumStr) {
+				roomHT = ht;
+				break;
+			}
+		}
+		if (roomHT == null) {
+			Utils.tr("ERROR","LayoutTiles.BuildRoom()", "Room not found: "+rNumStr);
+			return;
+		}
+		BuildRoom(roomHT);
+	}
+
 	// Build a room from an XML <room> entry
 	public void BuildRoom(PT_XMLHashtable room) {
+		//Destroy any old Tiles
+		foreach (Transform t in tileAnchor) { //Clear out old tiles
+			//^ You can iterate over a Transform to get its children
+			Destroy(t.gameObject);
+		}
+		
+		// Move the Mage out of the way
+		Mage.S.pos = Vector3.left * 1000;
+		// ^ This keeps the Mage from accidentally triggering OnTriggerExit() on
+		// a Portal. In my testing, I found that OnTriggerExit was being called
+		// at strange times.
+		Mage.S.ClearInput(); // Cancel any active mouse input and drags
+		
+		string rNumStr = room.att("num");
+
 		// Get the texture names for the floors and walls from <room> attributes
 		string floorTexStr = room.att("floor");
 		string wallTexStr = room.att("wall");
@@ -64,6 +114,8 @@ public class LayoutTiles : MonoBehaviour {
 		GameObject go;
 		int height;
 		float maxY = roomRows.Length-1;
+		List<Portal> portals = new List<Portal>();
+
 		// These loops scan through each tile of each row of the room
 		for (int y=0; y<roomRows.Length; y++) {
 			for (int x=0; x<roomRows[y].Length; x++) {
@@ -113,29 +165,108 @@ public class LayoutTiles : MonoBehaviour {
 				//Check for specific entities in the room
 				switch(rawType) {
 				case "X": //starting pos for the Mage
-					Mage.S.pos = ti.pos; //uses the mage singleton
+					//Mage.S.pos = ti.pos; //uses the mage singleton
+
+					if (firstRoom) {
+						Mage.S.pos = ti.pos; // Uses the Mage Singleton
+						roomNumber = rNumStr;
+						// ^ Setting roomNumber now keeps any portals from
+						// moving the Mage to them in this first room.
+						firstRoom = false;
+					}
+					break;
+					
+				case "0": // Numbers are room portals (up to F in hexadecimal)
+				case "1": // This allows portals to be placed in the Rooms.xml file
+				case "2":
+				case "3":
+				case "4":
+				case "5":
+				case "6":
+				case "7":
+				case "8":
+				case "9":
+				case "A":
+				case "B":
+				case "C":
+				case "D":
+				case "E":
+				case "F":
+					// Instantiate a Portal
+					GameObject pGO = Instantiate(portalPrefab) as GameObject;
+					Portal p = pGO.GetComponent<Portal>();
+					p.pos = ti.pos;
+					p.transform.parent = tileAnchor;
+					// ^ Attaching this to the tileAnchor means that the Portal
+					// will be Destroyed when a new room is built
+					p.toRoom = rawType;
+					portals.Add(p);
+					break;
+
+				default:
+					// Try to see if there's an Enemy for that letter
+					Enemy en = EnemyFactory(rawType);
+					if (en == null) break; // If there's not one, break out
+					// Set up the new Enemy
+					en.pos = ti.pos;
+					// Make en a child of tileAnchor so it's deleted when the
+					// next room is loaded.
+					en.transform.parent = tileAnchor;
+					en.typeString = rawType;
 					break;
 				}
-				// More to come here...
+
+
+				//More to come here...
 			}
 		}
+
+		// Position the Mage
+		foreach (Portal p in portals) {
+			// If p.toRoom is the same as the room number the Mage just exited,
+			// then the Mage should enter this room through this Portal
+			// Alternatively, if firstRoom == true and there was no X in the
+			// room (as a default Mage starting point), move the Mage to this
+			// Portal as a backup measure (if, for instance, you want to just
+			// load room number "5")
+			if (p.toRoom == roomNumber || firstRoom) {
+				// ^ If there's an X in the room, firstRoom will be set to false
+				//by the time the code gets here
+				Mage.S.StopWalking(); // Stop any Mage movement
+				Mage.S.pos = p.pos; // Move _Mage to this Portal location
+				// _Mage maintains her facing from the previous room, so there
+				// is no need to rotate her in order for her to enter this room
+				// facing the right direction.
+				p.justArrived = true;
+				//^ Tell the Portal that Mage has just arrived.
+				firstRoom = false;
+				//^ Stops a 2nd Portal in this room from moving the Mage to it
+			}
+		}
+		
+		// Finally assign the roomNumber
+		roomNumber = rNumStr;
 	}
-	// Build a room based on room number. This is an alternative version of
-	//  BuildRoom that grabs roomXML based on  num.
-	public void BuildRoom(string rNumStr) {
-		PT_XMLHashtable roomHT = null;
-		for (int i=0; i<roomsXML.Count; i++) {
-			PT_XMLHashtable ht = roomsXML[i];
-			if (ht.att("num") == rNumStr) {
-				roomHT = ht;
+
+	public Enemy EnemyFactory(string sType) {
+		// See if there's an EnemyDef with that sType
+		GameObject prefab = null;
+		foreach (EnemyDef ed in enemyDefinitions) {
+			if (ed.str == sType) {
+				prefab = ed.go;
 				break;
 			}
 		}
-		if (roomHT == null) {
-			Utils.tr("ERROR","LayoutTiles.BuildRoom()",
-			         "Room not found: "+rNumStr);
-			return;
+		if (prefab == null) {
+			Utils.tr("LayoutTiles.EnemyFactory()","No EnemyDef for: "+sType);
+			return(null);
 		}
-		BuildRoom(roomHT);
+		GameObject go = Instantiate(prefab) as GameObject;
+		// The generic form of GetComponent (with the <>) won't work for
+		// interfaces like Enemy, so we must use this form instead.
+		Enemy en = (Enemy) go.GetComponent(typeof(Enemy));
+		return(en);
 	}
+
+
 }
--- Assets/__Scripts/Mage.cs
+++ Assets/__Scripts/Mage.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic; //Enables List<>s
 using System.Linq; //Enables LING queries
+using UnityEngine.UI;
 
 //The MPhase enum is used to track the phase of mouse interaction
 public enum MPhase {
@@ -30,6 +31,7 @@ public class MouseInfo {
 	public RaycastHit hitInfo; //info abt what was hit by the ray
 	public bool hit; //Wheter the mouse was over any collider
 
+
 	//These methods see if the mouseRay hits nything
 	public RaycastHit Raycast() {
 		hit = Physics.Raycast (ray, out hitInfo);
@@ -51,6 +53,10 @@ public class Mage : PT_MonoBehaviour {
 	public float mDragDist = 5; //Min dist in pixels to be a drag
 
 	public float activeScreenWidth = 1; //% of the screen to use
+	
+	public HeartBar heartBar;
+
+	private Vector3 firstPos;
 
 	public float speed = 2; //mage walk speed
 
@@ -58,11 +64,50 @@ public class Mage : PT_MonoBehaviour {
 	public float elementRotDist = 0.5f; //Radihus of rotation
 	public float elementRotSpeed = 0.5f; //Period of rotation
 	public int maxNumSelectedElements = 1;
+	public Color[] elementColors; 
+
+	// These set the min and max distance between two line points
+	public float lineMinDelta = 0.1f;
+	public float lineMaxDelta = 0.5f;
+	public float lineMaxLength = 8f;
+
+	public GameObject fireGroundSpellPrefab;
+	public GameObject airGroundSpellPrefab;
+
+	public float health = 4; // Total mage health
+	public float damageTime = -100;
+	// ^ Time that damage occurred. It's set to -100 so that the Mage doesn't
+	// act damaged immediately when the scene starts
+	public float knockbackDist = 1; // Distance to move backward
+	public float knockbackDur = 0.5f; // Seconds to move backward
+	public float invincibleDur = 0.5f; // Seconds to be invincible
+	public int invTimesToBlink = 4; // # blinks while invincible
 
 	public bool _________________;
 
+	private bool invincibleBool = false; // Is Mage invincible?
+	private bool knockbackBool = false; // Mage being knocked back?
+	private Vector3 knockbackDir; // Direction of knockback
+	private Transform viewCharacterTrans;
+
+	protected Transform spellAnchor; // The parent transform for all spells
+	public float totalLineLength;
+	public List<Vector3> linePts; // Points to be shown in the line
+	protected LineRenderer liner; // Ref to the LineRenderer Component
+	protected float lineZ = -0.1f; // Z depth of the line
+	// ^ protected variables are between public and private.
+	//public variables can be seen by everyone
+	//private variables can only be seen by this class
+	//protected variables can be seen by this class or any subclasses
+	//only public variables appear in the Inspector
+	//(or those with [SerializeField] in the preceding line)
 	public MPhase mPhase = MPhase.idle;
 	public List<MouseInfo> mouseInfos = new List<MouseInfo>();
+	public string actionStartTag; // ["Mage", "Ground", "Enemy"]
+
+	public Text DeathText;
+	public Button RestartButton;
+	public Text RestartText;
 
 	public bool walking = false;
 
@@ -77,6 +122,20 @@ public class Mage : PT_MonoBehaviour {
 
 		//Find the characterTrans to rotate with Face()
 		characterTrans = transform.Find ("CharacterTrans");
+		viewCharacterTrans = characterTrans.Find("View_Character");
+
+		// Get the LineRenderer component and disable it
+		liner = GetComponent<LineRenderer>();
+		liner.enabled = false;
+
+		GameObject saGO = new GameObject("Spell Anchor");
+		// ^ Create an empty GameObject named "Spell Anchor". When you create a
+		// new GameObject this way, it's at P:[0,0,0] R:[0,0,0] S:[1,1,1]
+		spellAnchor = saGO.transform;//Get its transform
+
+		DeathText.enabled = false;
+		RestartButton.interactable = false;
+		RestartText.enabled = false;
 	}
 
 	void Update () {
@@ -178,30 +237,106 @@ public class Mage : PT_MonoBehaviour {
 	void MouseDown() {
 		//The mouse was pressed on something (it coudl be a drag or tap
 		if (DEBUG) print("Mage.MouseDown()");
+
+		GameObject clickedGO = mouseInfos[0].hitInfo.collider.gameObject;
+		// ^ If the mouse wasn't clicked on anything, this would throw an error
+		// because hitInfo would be null. However, we know that MouseDown()
+		// is only called when the mouse WAS clicking on something, so
+		//hitInfo is guaranteed to be defined.
+		
+		GameObject taggedParent = Utils.FindTaggedParent(clickedGO);
+		if (taggedParent == null) {
+			actionStartTag = "";
+		} else {
+			actionStartTag = taggedParent.tag;
+			//^ this should be either "Ground", "Mage", or "Enemy"
+		}
 	}
 
 	void MouseTap() {
 		//Something was tapped like a button
 		if(DEBUG) print("Mage.MouseTap()");
 
-		WalkTo(lastMouseInfo.loc); //Walk tot he latest mouseInfo pos
-		ShowTap(lastMouseInfo.loc); //show where the play ert tapped
+		// Now this cares what was tapped
+		switch (actionStartTag) {
+		case "Mage":
+			// Do nothing
+			break;
+		case "Ground":
+			// Move to tapped point @ z=0 whether or not an element is selected
+			WalkTo(lastMouseInfo.loc);  // Walk to the first mouseInfo pos
+			ShowTap(lastMouseInfo.loc); // Show where the player tapped
+			break;
+		}
 	}
 
 	void MouseDrag() {
 		//The mosue is being dragged across osmething
 		if(DEBUG) print("Mage.MouseDrag()");
 
-		//Continuously walk to ward the current mouseinfo pos
-		WalkTo(mouseInfos[mouseInfos.Count-1].loc);
+		// Drag is meaningless unless the mouse started on the ground
+		if (actionStartTag != "Ground") return;
+		
+		// If there is no element selected, the player should follow the mouse
+		if (selectedElements.Count == 0) {
+			// Continuously walk toward the current mouseInfo pos
+			WalkTo(mouseInfos[mouseInfos.Count-1].loc);
+		}else {
+			// This is a ground spell, so we need to draw a line
+			AddPointToLiner( mouseInfos[mouseInfos.Count-1].loc );
+			// ^ add the most recent MouseInfo.loc to liner
+		}
 	}
 
 	void MouseDragUp() {
 		//the mosue is released after being dragged
 		if(DEBUG) print("Mage.Mouse.DragUp()");
-		StopWalking();
+
+		// Drag is meaningless unless the mouse started on the ground
+		if (actionStartTag != "Ground") return;
+		
+		// If there is no element selected, stop walking now
+		if (selectedElements.Count == 0) {
+			// Stop walking when the drag is stopped
+			StopWalking();
+		}else {
+			CastGroundSpell(); 
+			
+			// Clear the liner
+			ClearLiner();
+		}
 	}
 
+	void CastGroundSpell() {
+		// There is not a no-element ground spell, so return
+		if (selectedElements.Count == 0) return;
+		
+		// Because this version of the prototype only allows a single element to
+		//  be selected, we can use that 0th element to pick the spell.
+		switch (selectedElements[0].type) {
+		case ElementType.fire:
+			GameObject fireGO;
+			foreach( Vector3 pt in linePts ) { // For each Vector3 in linePts...
+				// ...create an instance of fireGroundSpellPrefab
+				fireGO = Instantiate(fireGroundSpellPrefab) as GameObject;
+				fireGO.transform.parent = spellAnchor;
+				fireGO.transform.position = pt;
+			}
+			break;
+		case ElementType.air:
+			GameObject airGO;
+			foreach( Vector3 pt in linePts ) {
+				airGO = Instantiate (airGroundSpellPrefab) as GameObject;
+				airGO.transform.parent = spellAnchor;
+				airGO.transform.position = pt;
+			}
+			break;
+			//TODO: Add other elements types later
+		}
+		
+		// Clear the selectedElements; they're consumed by the spell
+		ClearElements();
+	}
 
 	//Walk to a specific position. The Position.z is always 0
 	public void WalkTo(Vector3 xTarget) {
@@ -226,6 +361,32 @@ public class Mage : PT_MonoBehaviour {
 	}
 
 	void FixedUpdate() { //happens every physics step 50 times /sec
+		this.transform.position = new Vector3 (this.transform.position.x,this.transform.position.y,-0.02f);
+		if (invincibleBool) {
+			// Get number [0..1]
+			float blinkU = (Time.time - damageTime)/invincibleDur;
+			blinkU *= invTimesToBlink; // Multiply by times to blink
+			blinkU %= 1.0f;
+			// ^ Modulo 1.0 gives us the decimal remainder left when dividing ?blinkU
+			// by 1.0. For example: 3.85f % 1.0f is 0.85f
+			bool visible = (blinkU > 0.5f);
+			if (Time.time - damageTime > invincibleDur) {
+				invincibleBool = false;
+				visible = true; // Just to be sure
+			}
+			// Making the GameObject inactive makes it invisible
+			viewCharacterTrans.gameObject.SetActive(visible);
+		}
+
+		if (knockbackBool) {
+			if (Time.time - damageTime > knockbackDur) {
+				knockbackBool = false;
+			}
+			float knockbackSpeed = knockbackDist/knockbackDur;
+			vel = knockbackDir * knockbackSpeed;
+			return; // Returns to avoid walking code below
+		}
+
 		if (walking) {///if mAge is walking
 			if ((walkTarget-pos).magnitude < speed*Time.fixedDeltaTime) {
 				//If mage is very close to walkTarget, just stop there
@@ -234,6 +395,7 @@ public class Mage : PT_MonoBehaviour {
 			} else {
 				//Otherwise, move toward walkTArget
 				this.GetComponent<Rigidbody>().velocity = (walkTarget-pos).normalized*speed;
+				this.transform.position = new Vector3 (this.transform.position.x,this.transform.position.y,-0.02f);
 			}
 		} else {
 			//If not walking, velocity should be zero
@@ -250,10 +412,57 @@ public class Mage : PT_MonoBehaviour {
 			if (ti.height > 0) {//IF ti.height is >0
 				//The this ti is a wall, and mage should stop
 				StopWalking();
-				Debug.Log ("TileTooHIGH");
 			}
 		}
+		// See if it's an EnemyBug
+		EnemyBug bug = coll.gameObject.GetComponent<EnemyBug>();
+		// If otherGO is an EnemyBug, pass bug to CollisionDamage(), which will
+		// interpret it as an Enemy
+		if (bug != null) CollisionDamage(bug);
+
+		// If otherGO is an EnemyBug, pass otherGO to CollisionDamage()
+		//if (bug != null) CollisionDamage(otherGO);
 	}
+
+	void OnTriggerEnter(Collider other) {
+		EnemySpiker spiker = other.GetComponent<EnemySpiker> ();
+		if (spiker != null) {
+			//CollisionDamage() will see spker as an Enemy
+			CollisionDamage (spiker);
+		}
+	}
+
+	void CollisionDamage(Enemy enemy) {
+		// Don't take damage if you're already invincible
+		if (invincibleBool) return;
+		// The Mage has been hit by an enemy
+		StopWalking();
+		ClearInput();
+		health -= enemy.touchDamage; // Take 1 point of damage (for now)
+		heartBar.ModifyHealth(-10);
+		if (health <= 0) {
+			Die();
+			return;
+		}
+		damageTime = Time.time;
+		knockbackBool = true;
+		knockbackDir = (pos - enemy.pos).normalized;
+		invincibleBool = true;
+	}
+	// The Mage dies
+	void Die() {
+		// Reload the level
+		// ^ Eventually, you'll want to do something more elegant
+		DeathText.enabled = true;
+		RestartButton.interactable = true;
+		RestartText.enabled = true;
+		this.gameObject.SetActive (false);
+	}
+
+	public void RestartLevel() {
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
 
 	//Show where the player tapped
 	public void ShowTap(Vector3 loc) {
@@ -281,6 +490,8 @@ public class Mage : PT_MonoBehaviour {
 		//^Note the typecast fromElementType to int in the line above
 		Element el = go.GetComponent<Element>();
 		el.transform.parent = this.transform;
+		go.GetComponent<Collider>().enabled = false;
+		go.layer = 2;
 
 		selectedElements.Add (el); //Add el to the list of selceted ELements
 	}
@@ -323,5 +534,82 @@ public class Mage : PT_MonoBehaviour {
 			vec.z = -0.5f;
 			el.lPos = vec; //Set the pos of Element_Sphere
 		}
+	}
+
+	//---------------- LineRenderer Code ----------------//
+	
+	// Add a new point to the line. This ignores the point if it's too close to
+	// existing ones and adds extra points if it's too far away
+	void AddPointToLiner(Vector3 pt) {
+		pt.z = lineZ; //Set the z of the pt to lineZ to elevate it slightly
+		//above the ground
+		
+		//linePts.Add(pt);
+		//UpdateLiner();
+
+		// Always add the point if linePts is empty...
+		if (linePts.Count == 0) {
+			linePts.Add (pt);
+			totalLineLength = 0;
+			return; // ...but wait for a second point to enable the LineRenderer
+		}
+		
+		// If the line is too long already, return
+		if (totalLineLength > lineMaxLength) return;
+		
+		// If there is a previous point (pt0), then find how far pt is from it
+		Vector3 pt0 = linePts[linePts.Count-1]; // Get the last point in linePts
+		Vector3 dir = pt-pt0;
+		float delta = dir.magnitude;
+		dir.Normalize();
+		
+		totalLineLength += delta;
+		
+		// If it's less than the min distance
+		if ( delta < lineMinDelta ) {
+			// ...then it's too close; don't add it
+			return;
+		}
+		
+		// If it's further than the max distance then extra points...
+		if (delta > lineMaxDelta) {
+			// ...then add extra points in between
+			float numToAdd = Mathf.Ceil(delta/lineMaxDelta);
+			float midDelta = delta/numToAdd;
+			Vector3 ptMid;
+			for (int i=1; i<numToAdd; i++) {
+				ptMid = pt0+(dir*midDelta*i);
+				linePts.Add(ptMid);
+			}
+		}
+		
+		linePts.Add(pt);// Add the point
+		UpdateLiner();// And finally update the line
+	}
+	
+	// Update the LineRenderer with the new points
+	public void UpdateLiner() {
+		// Get the type of the selectedElement
+		int el = (int) selectedElements[0].type;
+		
+		//Set the line color based on that type
+		liner.SetColors(elementColors[el],elementColors[el]);
+		
+		// Update the representation of the ground spell about to be cast
+		liner.SetVertexCount(linePts.Count);// Set the number of vertices
+		for (int i=0; i<linePts.Count; i++) {
+			liner.SetPosition(i, linePts[i]);// Set each vertex
+		}
+		liner.enabled = true;// Enable the LineRenderer
+	}
+	
+	public void ClearLiner() {
+		liner.enabled = false;// Disable the LineRenderer
+		linePts.Clear();// and clear all linePts
+	}
+
+	// Stop any active drag or other mouse input
+	public void ClearInput() {
+		mPhase = MPhase.idle;
 	}
 }
